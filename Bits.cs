8710efe0dce5e5dccce27e78c4d9a9a0e4416e87
IlyTom <126102373+IlyTom@users.noreply.github.com>
2024-04-19 22:55:41 +0300

    Добавьте файлы проекта.

---
+++ Bits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit
+{
+    internal class Bits
+    {
+        private byte value;
+
+        public Bits(byte value)
+        {
+            this.value = value;
+        }
+
+        public static implicit operator Bits(long value)
+        {
+            return new Bits((byte)value);
+        }
+
+        public static implicit operator Bits(int value)
+        {
+            return new Bits((byte)value);
+        }
+
+        public static implicit operator Bits(byte value)
+        {
+            return new Bits(value);
+        }
+        public override string ToString()
+        {
+            return $"Bits: {this.value}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Bit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long longValue = 123;
+            int intValue = 45;
+            byte byteValue = 10;
+
+            Bits bitsFromLong = longValue;
+            Bits bitsFromInt = intValue;
+            Bits bitsFromByte = byteValue;
+
+            
+
+            Console.WriteLine(longValue);
+            Console.WriteLine((byte)10);
+        }
+    }
+}
